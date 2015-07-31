dd5132436993306919630acb87aa702a46bcb2bf
Indigo Orton <indigo@xperiel.com>
2015-07-31 09:34:34 -0700

    Enable custom number of commits to rebase

--- git-rebase-only-latest.sh
+++ git-rebase-only-latest.sh
@@ -4,6 +4,8 @@ set -o nounset
 
 GITBRANCH=$(git branch 2>/dev/null | sed -n 's/^\*\ //p')
 UPSTREAM=`git rev-parse --abbrev-ref --symbolic-full-name @{u}`
+NUMBER_OF_COMMITS="$1"
+
 if [ -z "$GITBRANCH" ]; then
   echo "No git branch found"
   exit 1
@@ -12,5 +14,12 @@ if [ -z "$UPSTREAM" ]; then
   echo "No upstream found"
   exit 1
 fi
+if [ -z "$NUMBER_OF_COMMITS" ]; then
+  NUMBER_OF_COMMITS=1
+fi
+if (( NUMBER_OF_COMMITS < 0 )); then
+  echo "Invalid number of commits: $NUMBER_OF_COMMITS. Must be a positive number"
+  exit 1
+fi
 
-git rebase --onto $UPSTREAM $GITBRANCH~1 $GITBRANCH
+git rebase --onto $UPSTREAM $GITBRANCH~$NUMBER_OF_COMMITS $GITBRANCH
